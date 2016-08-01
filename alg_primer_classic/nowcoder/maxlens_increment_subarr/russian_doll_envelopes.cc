792676d3a82ebbea8e9011927976905544429a3d
ZacharyWiZ <wizzhangquan@gmail.com>
2016-08-01 15:33:36 +0800

    accept the leetcode 354

--- alg_primer_classic/nowcoder/maxlens_increment_subarr/russian_doll_envelopes.cc
+++ alg_primer_classic/nowcoder/maxlens_increment_subarr/russian_doll_envelopes.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <iterator>
+#include <numeric>
 #include <vector>
 
 /*
@@ -19,6 +20,53 @@
 using namespace std;
 
 
+//如果需要提交，只需要将此类的代码提交上去即可
+class Solution {
+public:
+    Solution() = default;
+
+    int maxEnvelopes(vector<pair<int, int> >& envelopes) {
+        if (envelopes.empty()) return 0;
+
+        sort(envelopes.begin(), envelopes.end(),
+            Solution::increment_compare);
+
+        vector<int> help_arr;
+        int longest = 1;
+        help_arr.push_back(envelopes.begin()->second);
+        for (vector<pair<int, int> >::iterator iter = envelopes.begin();
+            iter != envelopes.end(); ++iter) {
+            int h = iter->second;
+            vector<int>::iterator bigger_iter =
+                find_if(help_arr.begin(), help_arr.end(), 
+                    [h](int &n) -> bool { return n>=h; });
+
+            int position = bigger_iter - help_arr.begin();
+            cout << "find bigger help_arr[" 
+                 << position
+                 << "]:" << *bigger_iter << endl;
+            
+            if (bigger_iter == help_arr.end())
+                help_arr.push_back(h);
+            else
+                *bigger_iter = h;
+            
+            longest = max(longest, position + 1);
+        }
+        return longest;
+
+    }
+    //这里必须是static，不然传入sort会发生错误
+    //如果是本类非static成员函数，会有一个隐藏的函数参数this
+    static bool increment_compare(const pair<int, int> &a, 
+        const pair<int, int> &b) {
+        //important
+        if (a.first != b.first) //w升序
+            return a.first < b.first;
+        else //w相等情况下, h降序
+            return a.second > b.second; 
+    }
+};
 
 class Env_datas {
     
@@ -52,41 +100,28 @@ public:
         cout << endl;
     }
 
-    //这里必须是static，不然传入sort会发生错误
-    //如果是本类非static成员函数，会有一个隐藏的函数参数this
-    static bool increment_compare(const env &a, const env &b) {
-        //important
-        if (a.first != b.first) //w升序
-            return a.first < b.first;
-        else //w相等情况下, h降序
-            return a.second > b.second; 
-    }
-
     void sort_envs() {
         sort(envelopes_.begin(), envelopes_.end(),
-            Env_datas::increment_compare);
+            Solution::increment_compare);
     }
 
 private:
     vector<env > envelopes_;
 };
 
-//@UNIT_TEST : Env_datas sort
+
+//@UNIT_TEST : Env_datas Solution
 void test_envs(void) {
+    Solution s;
     while(true) { 
         Env_datas envs;
         envs.in_envs();
-        envs.sort_envs();
-        envs.show_envs();
+
+        cout << "get the max Envs's lens: "
+             << s.maxEnvelopes(envs.get_data())
+             << "-----------------" << endl;
     }
 }
-
-class Solution {
-public:
-    int maxEnvelopes(vector<pair<int, int> >& envelopes) {
-
-    }
-};
 
 int main() {
     test_envs();
