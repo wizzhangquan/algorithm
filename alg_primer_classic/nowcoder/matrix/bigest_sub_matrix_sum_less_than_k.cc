148e3d14524a8482c50fe16c7de9efe3ae7b8307
ZacharyWiZ <wizzhangquan@gmail.com>
2016-08-05 17:38:54 +0800

    getLongestSubArrSumLessThanK

--- alg_primer_classic/nowcoder/matrix/bigest_sub_matrix_sum_less_than_k.cc
+++ alg_primer_classic/nowcoder/matrix/bigest_sub_matrix_sum_less_than_k.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 /*
  * 给定一个无序矩阵，其中有正有负有0，再给定一个值k，
@@ -10,8 +11,73 @@ using namespace std;
 
 class FindSubMatrix {
 public:
+    int getBiggestSubMatrix(const vector<vector<int>> &matrix, int k) {
+        if (matrix.empty()) return 0;
+        
+        k_ = k;
+        vector<int> sumRow;
+        for (int i=0; i<matrix.size(); ++i) {
+            for (int j=i; j<matrix.size(); ++j) {
+                const vector<int> &curRow = matrix[j];
+                if (j == i)
+                    sumRow.assign(curRow.begin(), curRow.end());
+                else 
+                    for (int ci=0; ci!=curRow.size(); ++ci) sumRow[ci] += curRow[ci];
+                
+            }
+        }
+    }
+
+    void test_getLongestSubLess(void) {
+        cout << "please cin arr elements:" << endl;
+        int n;
+        vector<int> arr;
+        while (cin>>n) arr.push_back(n);
+        cout << "please cin k:";
+        cin >> k_;
+        auto range = getLongestSubLess(arr);
+        cout << "now show the subarr which sum <= " << k_
+             << ":" << endl;
+        for (int i=range.first; i!=range.second; ++i)
+            cout << arr[i] << " ";
+        cout << endl;
+    }
 
 
+private:
+    //@return : longest lens of sub arr which sum less than k_
+    pair<int, int> getLongestSubLess(const vector<int> &arr) {
+        const int arrsz = arr.size();
+        pair<int, int> range(0, 0);
+
+        if (arrsz == 0) return range;
+
+        if (arrsz == 1)
+            if (arr[0] <= k_) return make_pair(0,1);
+            else return range;
+ 
+        vector<int> helpArr;//储存当前i的最大连续子序列和
+        int sum = arr[0], diff, lens;
+        helpArr.push_back(arr[0]);
+        for (int i=1; i!=arrsz; ++i) {
+            sum += arr[i];
+            helpArr.push_back(max(helpArr.back(), sum));
+            diff = sum - k_;
+            auto first_i = find_if(begin(helpArr), end(helpArr), 
+                    [diff](const int &n) -> bool {return n>=diff;});
+            int first_site = first_i - helpArr.end();
+            if (i+1-first_site > range.second-range.first) {
+                range.first = first_site;
+                range.second = i+1;
+            }
+        }
+        return range;
+    }
+
+    pair<int, int> position;
+    int length;
+    int width;
+    int k_;
 };
 
 class InOutData {
