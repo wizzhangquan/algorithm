54672ce47ba663f79553d5cbb1e773204f2089ff
ZacharyWiZ <wizzhangquan@gmail.com>
2016-08-11 10:49:35 +0800

    do not use reference

--- alg_primer_classic/nowcoder/recursion/reverse_stack.cc
+++ alg_primer_classic/nowcoder/recursion/reverse_stack.cc
@@ -36,7 +36,7 @@ public:
     }
 
     void test_getAndRemoveLast() {
-        T last = getAndRemoveLast(stk_);
+        T &last = getAndRemoveLast(stk_);
         cout << "the bottom of stack: "
              << last << endl;
         cout << "now the stack's top: "
@@ -44,7 +44,8 @@ public:
         cout << "print stack: ";
         printStack(stk_);
         cout << endl;
-        cout << "print stack: ";
+        last = 999;
+        cout << "change ret, print stack: ";
         printStack(stk_);
         cout << endl;
     }
@@ -75,12 +76,36 @@ public:
     T getAndRemoveLast(stack<T> &stk) {
         if (stk.empty())
             throw out_of_range("stk is empty");
-        T curTop = stk.top();
+        T curTop = stk.top(); 
+        //T &curTop = stk.top();
+        //这里不能使用引用
+        //因为是指向stk的栈中的某位置的
+        //如果返回后，到递归第二层之后，结果就不是原来的栈底
+        //而变成了之前的栈底之上的那个元素
+        //
+        //@localhost recursion]# ./a.out
+        //please enter stack num:
+        //5 4 3 2 1
+        //
+        //1 2 3 4 5 stk.empty ret:5
+        //ret last : 5
+        //ret last : 4
+        //ret last : 4
+        //ret last : 4
+        //the bottom of stack: 4
+        //now the stack's top: 1
+        //print stack: 1=> 2=> 3=> 4=>
+        //change ret to 999, print stack: 1=> 2=> 3=> 999=>
+        //
+        cout << curTop << " ";
         stk.pop();
-        if (stk.empty())
+        if (stk.empty()) {
+            cout << "stk.empty ret:" << curTop<< endl;
             return curTop;
+        }
         else {
             T ret = getAndRemoveLast(stk);
+            cout << "ret last : " << ret << endl;
             stk.push(curTop);
             return ret;
         }
