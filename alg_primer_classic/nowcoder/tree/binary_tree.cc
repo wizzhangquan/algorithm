c8f0b250b7dcf809644164ab7ad563a6d6f0a5ef
ZacharyWiZ <wizzhangquan@gmail.com>
2016-09-16 21:10:44 +0800

    binary tree class

---
+++ alg_primer_classic/nowcoder/tree/binary_tree.cc
@@ -0,0 +1,73 @@
+#include "binary_tree.h"
+#include <iostream>
+
+using namespace std;
+
+template <typename T, unsigned N>
+Btnode<T> *initBinaryTree(const T elements[N], int begin = 0) {
+    if (begin<0 || begin>=N || elements[begin]==-1)
+        return NULL;
+    Btnode<T> *node = new Btnode<T>(elements[begin]);
+    node->lchild = initBinaryTree(elements, 2*begin+1);
+    node->rchild = initBinaryTree(elements, 2*begin+2);
+    return node;
+}
+
+template <typename Type>
+Btnode<Type> *initTree(const Type elements[], const int sz, int begin = 0) {
+    if (begin<0 || begin>=sz || elements[begin]==-1)
+        return NULL;
+    Btnode<Type> *node = new Btnode<Type>(elements[begin]);
+    node->lchild = initTree(elements, sz, 2*begin+1);
+    node->rchild = initTree(elements, sz, 2*begin+2);
+    /*
+    cout << "init node ,data:" << node->data_;
+    if (node->lchild == NULL)
+        cout << " left:NULL";
+    else 
+        cout << " left:" << node->lchild->data_;
+
+    if (node->rchild == NULL)
+        cout << " right:NULL";
+    else 
+        cout << " right:" << node->lchild->data_;
+    cout << endl;
+    */
+    return node;
+}
+
+template <typename Type>
+void destoryTree(Btnode<Type> *root) {
+    if (root == NULL) return ;
+    destoryTree(root->lchild);
+    destoryTree(root->rchild);
+    delete root;
+}
+
+template <typename Type>
+void preorder_traversal(const Btnode<Type> *root) {
+    if (root == NULL) return ;
+    cout << root->data_ << " ";
+    preorder_traversal(root->lchild);
+    preorder_traversal(root->rchild);
+}
+
+template <typename Type>
+void midorder_traversal(const Btnode<Type> *root) {
+    if (root == NULL) return ;
+    midorder_traversal(root->lchild);
+    cout << root->data_ << " ";
+    midorder_traversal(root->rchild);
+}
+
+int main() {
+    int arr[9] = {1, 2, 3, 4, -1, 6, 7, -1, 9};
+
+    Btnode<int> *root = initTree<int>(arr, 9);
+
+    cout << "begin midorder traversal" << endl;
+    midorder_traversal(root);
+    cout << endl;
+    destoryTree(root);
+    return 0;
+}
---
+++ alg_primer_classic/nowcoder/tree/binary_tree.h
@@ -0,0 +1,54 @@
+#ifndef __BINARY_TREE_H__
+#define __BINARY_TREE_H__
+
+#include <utility>
+#include <cstddef>
+
+template <typename Type>
+class Btnode;
+
+template <typename Type>
+Btnode<Type> *initTree(const Type elements[], const int sz, int begin = 0);
+
+template <typename Type>
+void destoryTree(Btnode<Type> *root);
+
+template <typename Type>
+void preorder_traversal(const Btnode<Type> *root);
+
+template <typename Type>
+void midorder_traversal(const Btnode<Type> *root);
+
+template <typename Type>
+class Btnode {
+
+    template <typename T, unsigned N>
+    friend Btnode<T> *initBinaryTree(const T elements[N], int begin = 0);
+    //这种方式为模板类的非约束模板友元函数
+    //在类内部声明友元函数模板，可以创建非约束友元函数，
+    //即每个函数具体化都是每个类具体化的友元
+
+    friend Btnode<Type> *initTree<Type>(const Type elements[], const int sz, int begin); 
+    //模板类的约束模板友元函数 可以这样写，
+    //但是需要前置声明该模板函数，并且不可以再使用非类型模板函数
+    //eg:template <unsigned N>这种了 ==> error
+    //另外，函数名后面的<Type>可以为<>，这是因为可以从函数参数推断出模板类型
+    //并且这里，begin不能有默认参数，毕竟不是第一次声明
+
+    friend void destoryTree<>(Btnode<Type> *root);
+    friend void preorder_traversal<>(const Btnode<Type> *root);
+    friend void midorder_traversal<>(const Btnode<Type> *root);
+
+public:
+    Btnode(Type data = 0) : 
+        data_(data), lchild(NULL), rchild(NULL) { 
+    }
+
+    //~Btnode() { lchild = rchild = NULL; }
+private:
+    Type data_;
+    Btnode *lchild;
+    Btnode *rchild;
+};
+
+#endif
